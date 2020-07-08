906ea4ba8bff492162a7e998f893178ad0e5935a
SangdonLee972 <dltkdehstm12@naver.com>
2020-07-08 21:17:49 +0900

    던전만들기 1일차

---
+++ Dodge 2020 6 29/Assets/Dodge/Skript/PlayerControloer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControloer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody m_Rigidbody;
+
+    public float m_Speed = 25f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /* GameObject.*/ GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * 10);
+
+        
+    }
+}
