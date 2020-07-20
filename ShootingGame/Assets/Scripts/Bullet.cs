cc65455bacee5ef35117d117c067a29bc1143149
SangdonLee972 <dltkdehstm12@naver.com>
2020-07-20 21:11:07 +0900

    7월 20일 월요일 슈팅게임만들기

---
+++ ShootingGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    bool Istrue = false;
+    public float Speed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = transform.up * Speed;
+        Vector3 movement = transform.up * Speed * Time.deltaTime;
+        transform.position += movement;
+        
+
+    }
+}
---
+++ ShootingGame/Assets/Scripts/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    float Speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1,0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        }
+
+    }
+}
