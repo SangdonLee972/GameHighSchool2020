049e91be7e5a3fe866a4d76e04940a5309aa93ad
SangdonLee972 <dltkdehstm12@naver.com>
2020-07-21 21:26:40 +0900

    슈팅게임2일차

---
+++ ShootingGame/Assets/ShootingGame/Scripts/Enemy_Small.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Small : MonoBehaviour
+{
+    public float Speed = 3;
+    void Update()
+    {
+        Vector3 velocity = transform.up * Speed;
+        Vector3 movement = transform.up * Speed * Time.deltaTime;
+        transform.position -= movement;
+
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_Speed = 1f;
+    public Bullet m_BulletPrefab;
+    public float m_AttackDelay = 0.5f;
+    public float m_AttackCooldown = 0f;
+
+    public Transform[] m_FireMuzzles;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector2 inputValue = new Vector2(xAxis, yAxis).normalized;
+        Vector3 movement = inputValue * m_Speed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.Space) && m_AttackCooldown <=0)
+        {
+            foreach(var fireMuzzle in m_FireMuzzles)
+            {
+                var bullet = GameObject.Instantiate(m_BulletPrefab,
+                fireMuzzle.position, fireMuzzle.rotation);
+            }
+            m_AttackCooldown = m_AttackDelay;
+        }
+        m_AttackCooldown -= Time.deltaTime;
+    }
+}
