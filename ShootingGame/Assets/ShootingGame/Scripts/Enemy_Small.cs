5394558d650192f4c916e0770c465a3e81c41d95
SangdonLee972 <dltkdehstm12@naver.com>
2020-07-22 20:59:59 +0900

    슈팅게임 3일차

--- ShootingGame/Assets/ShootingGame/Scripts/Enemy_Small.cs
+++ ShootingGame/Assets/ShootingGame/Scripts/Enemy_Small.cs
@@ -1,15 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Enemy_Small : MonoBehaviour
 {
-    public float Speed = 3;
+    public float m_Speed = 3f;
+
+    public Bullet m_Bullet;
+
+    public Transform[] m_FireMulzzles;
+
+    public float m_AttackDelay = 3f;
+    public float m_AttackCooldown = 0f;
+
+    public bool isDead = false;
+
     void Update()
     {
-        Vector3 velocity = transform.up * Speed;
-        Vector3 movement = transform.up * Speed * Time.deltaTime;
+        //이동
+        Vector3 velocity = transform.up * m_Speed;
+        Vector3 movement = velocity * Time.deltaTime;
         transform.position -= movement;
 
+        if (!isDead && m_AttackCooldown <= 0)
+        {
+            //총알 발사
+            foreach (var fireMulzzles in m_FireMulzzles)
+            {
+                GameObject.Instantiate(m_Bullet, fireMulzzles.position, fireMulzzles.rotation);
+                m_AttackCooldown = m_AttackDelay;
+            }
+        }
+        m_AttackCooldown -= Time.deltaTime;
+    }
+
+
+    public Animator m_Animator;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_Animator.SetBool("Die", true);
+        isDead = true;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
